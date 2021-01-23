352d3364859f3ef631906bf8ccb8b8897abed84f
katorzhin <alex.katorzhin@gmail.com>
2021-01-23 12:52:08 +0200

    Initial commit

---
+++ src/main/java/com/project/java/katorzhin/projectonlinestore/ProjectOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package com.project.java.katorzhin.projectonlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectOnlineStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/java/katorzhin/projectonlinestore/ProjectOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.java.katorzhin.projectonlinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
