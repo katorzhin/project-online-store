7c3b39a6c93f8ee396e59e8eeee08b4044d904c0
katorzhin <alex.katorzhin@gmail.com>
2021-02-13 23:52:22 +0200

    updated FinishPurchase

--- src/main/java/com/project/java/katorzhin/projectonlinestore/controller/dto/FinishPurchaseRequest.java
+++ src/main/java/com/project/java/katorzhin/projectonlinestore/controller/dto/FinishPurchaseRequest.java
@@ -1,23 +1,30 @@
 package com.project.java.katorzhin.projectonlinestore.controller.dto;
 
 import lombok.Data;
-import lombok.NonNull;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.Map;
 
 @Data
 public class FinishPurchaseRequest {
-    @NonNull
+    @NotNull
     private Map<Integer, Integer> productIdProductCount;
+
     @NotEmpty
     private String userName;
+
     private String userSurName;
+
     @NotEmpty
     private String email;
+
     @NotEmpty
     private String phone;
+
     @NotEmpty
     private String address;
+
     private String comment;
+
 }
---
+++ src/main/java/com/project/java/katorzhin/projectonlinestore/controller/dto/FinishPurchaseResponse.java
@@ -0,0 +1,11 @@
+package com.project.java.katorzhin.projectonlinestore.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FinishPurchaseResponse {
+
+    private Integer orderId;
+}
